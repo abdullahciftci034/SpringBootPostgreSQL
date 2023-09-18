40d07a2573a907878a51572c03ea9a193744165c
Abdullah Çiftçi <abdullahciftci034@gmail.com>
2023-09-18 09:56:28 +0300

    first commit

--- src/main/java/com/abdullah/SpringBootPostgreSQLApplication.java
+++ src/main/java/com/abdullah/SpringBootPostgreSQLApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootPostragreSQLApplication {
+public class SpringBootPostgreSQLApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootPostragreSQLApplication.class,args);
+        SpringApplication.run(SpringBootPostgreSQLApplication.class,args);
     }
 }
